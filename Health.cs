bbfc81b5591edc69bd600ee1a51d25bbe34c3fa2
seesonrise <89902479+seesonrise@users.noreply.github.com>
2023-03-25 16:08:08 +0200

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+	[SerializeField][HideInInspector] protected float _maxHealth = 100f;
+	[SerializeField][HideInInspector] protected float _health = 100f;
+
+	[SerializeField] public float health
+	{
+		get => _health;
+		set { if (value <= _maxHealth && value >= 0) _health = value; }
+	}
+	[SerializeField] public void DoDamage(float Damage)
+	{
+		if (Damage >= _maxHealth)
+		{
+			_health = 0;
+		}
+		else
+		{
+			_health -= Damage;
+		}
+	}
+	[SerializeField] public float maxhealth
+	{
+		get => maxhealth;
+		set => maxhealth = value;
+	}
+}
