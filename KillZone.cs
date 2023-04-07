2f34b13fad8320ba0933491eaa249c4a0bebb792
seesonrise <89902479+seesonrise@users.noreply.github.com>
2023-04-07 19:43:29 +0300

    Update KillZone.cs

--- KillZone.cs
+++ KillZone.cs
@@ -6,6 +6,8 @@ public class KillZone : MonoBehaviour
     [SerializeField] private LayerMask _playerLayer;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.layer == _playerLayer){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
