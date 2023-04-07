b2ab0d881848451394cb64e703816cf9d04a5a99
seesonrise <89902479+seesonrise@users.noreply.github.com>
2023-04-07 19:51:29 +0300

    Add files via upload

--- KillZone.cs
+++ KillZone.cs
@@ -6,8 +6,9 @@ public class KillZone : MonoBehaviour
     [SerializeField] private LayerMask _playerLayer;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == _playerLayer){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (collision.transform.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
