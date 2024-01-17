d1ab328e244bfecdce43726d986733bea7040d16
seesonrise <89902479+seesonrise@users.noreply.github.com>
2024-01-17 20:13:32 +0200

    Fix Building

--- Move.cs
+++ Move.cs
@@ -1,94 +1,96 @@
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 internal enum TypeMove { StandartMove, CellMove }
 [RequireComponent(typeof(Rigidbody2D))]
 public class Move : MonoBehaviour
 {
-	private Rigidbody2D _rb;
-	[SerializeField] internal TypeMove TypeMove;
+    private Rigidbody2D _rb;
+    [SerializeField] internal TypeMove TypeMove;
 
-	#region CellMove
-	[SerializeField] internal Vector2 CellSize;
-	[SerializeField] internal float SmoothTime;
-	[SerializeField] internal LayerMask WallLayer;
-	#endregion
+    #region CellMove
+    [SerializeField] internal Vector2 CellSize;
+    [SerializeField] internal float SmoothTime;
+    [SerializeField] internal LayerMask WallLayer;
+    #endregion
 
-	#region StandarMove
-	[SerializeField] internal float Speed;
-	[SerializeField] internal float JumpForce;
-	[SerializeField] internal GameObject GroundBox;
-	[SerializeField] internal Vector2 BoxSize;
-	[SerializeField] internal LayerMask GroundLayer;
-	private float InputX;
-	private float InputY;
-	#endregion
-	private void Awake()
-	{
-		_rb = GetComponent<Rigidbody2D>();
-	}
+    #region StandarMove
+    [SerializeField] internal float Speed;
+    [SerializeField] internal float JumpForce;
+    [SerializeField] internal GameObject GroundBox;
+    [SerializeField] internal Vector2 BoxSize;
+    [SerializeField] internal LayerMask GroundLayer;
+    private float InputX;
+    private float InputY;
+    #endregion
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
 
-	private void CellMove()
-	{
-		_rb.bodyType = RigidbodyType2D.Static;
-		if (Input.GetKeyDown(KeyCode.D) && !Physics2D.Raycast(transform.position, Vector2.right, 1f, WallLayer))
-		{
-			transform.position = new Vector2(transform.position.x + CellSize.x, transform.position.y);
-		}
-		if (Input.GetKeyDown(KeyCode.A) && !Physics2D.Raycast(transform.position, Vector2.left, 1f, WallLayer))
-		{
-			transform.position = new Vector2(transform.position.x - CellSize.x, transform.position.y);
-		}
-		if (Input.GetKeyDown(KeyCode.W) && !Physics2D.Raycast(transform.position, Vector2.up, 1f, WallLayer))
-		{
-			transform.position = new Vector2(transform.position.x, transform.position.y + CellSize.y);
-		}
-		if (Input.GetKeyDown(KeyCode.S) && !Physics2D.Raycast(transform.position, Vector2.down,1f,WallLayer))
-		{
-			transform.position = new Vector2(transform.position.x, transform.position.y - CellSize.y);
-		}
-	}
-	private void StandartMove()
-	{
-		Moving();
-		if (Input.GetButton("Jump") && DoGrounded())
-		{
-			Jump();
-		}
-	}
-	private bool DoMove(Vector2 dir)
-	{
-		Debug.DrawLine(transform.localPosition, dir, Color.green);
+    private void CellMove()
+    {
+        _rb.bodyType = RigidbodyType2D.Static;
+        if (Input.GetKeyDown(KeyCode.D) && !Physics2D.Raycast(transform.position, Vector2.right, 1f, WallLayer))
+        {
+            transform.position = new Vector2(transform.position.x + CellSize.x, transform.position.y);
+        }
+        if (Input.GetKeyDown(KeyCode.A) && !Physics2D.Raycast(transform.position, Vector2.left, 1f, WallLayer))
+        {
+            transform.position = new Vector2(transform.position.x - CellSize.x, transform.position.y);
+        }
+        if (Input.GetKeyDown(KeyCode.W) && !Physics2D.Raycast(transform.position, Vector2.up, 1f, WallLayer))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + CellSize.y);
+        }
+        if (Input.GetKeyDown(KeyCode.S) && !Physics2D.Raycast(transform.position, Vector2.down, 1f, WallLayer))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - CellSize.y);
+        }
+    }
+    private void StandartMove()
+    {
+        Moving();
+        if (Input.GetButton("Jump") && DoGrounded())
+        {
+            Jump();
+        }
+    }
+    private bool DoMove(Vector2 dir)
+    {
+        Debug.DrawLine(transform.localPosition, dir, Color.green);
 
-		if (Physics2D.Raycast(transform.position, dir).transform.gameObject.layer == WallLayer.value)
-		{
-			return false;
-		}
-		return true;
-	}
-	private bool DoGrounded()
-	{
-		return Physics2D.OverlapBox(GroundBox.transform.position, BoxSize, 0, GroundLayer) != null;
-	}
+        if (Physics2D.Raycast(transform.position, dir).transform.gameObject.layer == WallLayer.value)
+        {
+            return false;
+        }
+        return true;
+    }
+    private bool DoGrounded()
+    {
+        return Physics2D.OverlapBox(GroundBox.transform.position, BoxSize, 0, GroundLayer) != null;
+    }
     private void Moving()
-	{
+    {
         _rb.velocity = new Vector2(Speed * InputX, _rb.velocity.y);
     }
-	private void InputHandler()
-	{
-		InputX = Input.GetAxis("Horizontal");
-		InputY = Input.GetAxis("Vertical");
-	}
-	
-	private void Jump()
-	{
-		_rb.velocity = new Vector2(_rb.velocity.x,JumpForce);
-	}
+    private void InputHandler()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputY = Input.GetAxis("Vertical");
+    }
+
+    private void Jump()
+    {
+        _rb.velocity = new Vector2(_rb.velocity.x, JumpForce);
+    }
     private void Update()
     {
         if (TypeMove == TypeMove.CellMove)
         {
-			CellMove();
+            CellMove();
         }
         if (TypeMove == TypeMove.StandartMove)
         {
@@ -106,43 +108,44 @@ public class Move : MonoBehaviour
         for (int i = 0; i < maxLayers; i++)
         {
             string layerName = LayerMask.LayerToName(i);
-			if (layerName != "\r\n") { layerNames[i] = layerName; }
+            if (layerName != "\r\n") { layerNames[i] = layerName; }
         }
 
         return layerNames;
     }
     private void OnDrawGizmosSelected()
     {
-		Gizmos.color = Color.green;
-		Gizmos.DrawWireCube(GroundBox.transform.position, BoxSize);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(GroundBox.transform.position, BoxSize);
     }
 }
-
-[CustomEditor(typeof(Move)), CanEditMultipleObjects]
-public class MoveEditor: Editor
+#if UNITY_EDITOR
+[CustomEditor(typeof(Move))]
+public class MoveEditor : Editor
 {
-	public override void OnInspectorGUI()
-	{
-		var move = (Move)target;
+    public override void OnInspectorGUI()
+    {
+        var move = (Move)target;
         string[] options = move.GetLayerNames();
         move.TypeMove = (TypeMove)EditorGUILayout.EnumPopup("Select type move", move.TypeMove);
-		switch (move.TypeMove)
-		{
-			case TypeMove.StandartMove:
-				move.Speed = EditorGUILayout.Slider("Speed", move.Speed, 1f, 20f);
-				move.JumpForce = EditorGUILayout.Slider("Jump Force", move.JumpForce, 1f, 20f);
+        switch (move.TypeMove)
+        {
+            case TypeMove.StandartMove:
+                move.Speed = EditorGUILayout.Slider("Speed", move.Speed, 1f, 20f);
+                move.JumpForce = EditorGUILayout.Slider("Jump Force", move.JumpForce, 1f, 20f);
                 if (move.GroundBox == null)
                     move.GroundBox = new GameObject("Ground Box");
-                move.GroundLayer= (LayerMask)EditorGUILayout.MaskField("Ground Layer", move.GroundLayer, options);
+                move.GroundLayer = (LayerMask)EditorGUILayout.MaskField("Ground Layer", move.GroundLayer, options);
                 move.GroundBox = (GameObject)EditorGUILayout.ObjectField(move.GroundBox, typeof(GameObject), true);
                 move.BoxSize = EditorGUILayout.Vector2Field("Ground Box Size", move.BoxSize);
-				break;
+                break;
 
-			case TypeMove.CellMove:
-				move.CellSize = EditorGUILayout.Vector2Field("Cell Size", move.CellSize);
-				move.SmoothTime = EditorGUILayout.Slider("Smooth Time", move.SmoothTime, 0f, 1f);
+            case TypeMove.CellMove:
+                move.CellSize = EditorGUILayout.Vector2Field("Cell Size", move.CellSize);
+                move.SmoothTime = EditorGUILayout.Slider("Smooth Time", move.SmoothTime, 0f, 1f);
                 move.WallLayer = (LayerMask)EditorGUILayout.MaskField("Wall Layer", move.WallLayer, options);
                 break;
-		}
-	}
+        }
+    }
 }
+#endif
